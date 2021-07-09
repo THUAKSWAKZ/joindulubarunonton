caaff0f7fa80627e26a9f9a37cbeb01c545723e1
THUAKSWAKZ <87125474+THUAKSWAKZ@users.noreply.github.com>
2021-07-09 13:35:29 +0700

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -94,14 +94,14 @@ async def start_command(client: Client, message: Message):
 
 @Bot.on_message(filters.command('start') & filters.private)
 async def not_joined(client: Client, message: Message):
-    text = "<b>Join Dulu lah sayang\nLu kira gampang bikin ginian\nSusah gblk\n\nGada yang gratis\nSubs buru biar kita sama sama untung\n\nGak Join Mandul</b>"
+    text = "<b>Maap, akses nonton vidio ditolak\nUntuk menonton vidio di bot ini,anda harus join channel dengan menekan tombol dibawah ini lalu klik bergabung/join.lalu coba lagi and try again</b>"
     message_text = message.text
     try:
         command, argument = message_text.split()
         text = text + f" <b>and <a href='https://t.me/{client.username}?start={argument}'>try again</a></b>"
     except ValueError:
         pass
-    reply_markup = InlineKeyboardMarkup([[InlineKeyboardButton("BURU JOIN", url = client.invitelink)]])
+    reply_markup = InlineKeyboardMarkup([[InlineKeyboardButton("JOIN DISINI SAYANG", url = client.invitelink)]])
     await message.reply(
         text = text,
         reply_markup = reply_markup,
